53516433a04f0516e102f502edd45d9e7ddf9aeb
IwoSzczepaniak <iszczepaniak@student.agh.edu.pl>
2023-03-25 18:27:13 +0100

    lab2

---
+++ python_course2/tts/tts.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import pyttsx3 as tts
+
+end = None
+
+
+def main():
+    engine = tts.init()
+    engine.setProperty('volume', 0.7)
+    engine.setProperty('rate', 190)
+
+    while True:
+        tekst = input('>>')
+        engine.say(tekst)
+        engine.runAndWait()
+
+
+################################################################################
+
+main()
+
+################################################################################
